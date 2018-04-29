434db61bcff515e75e586d5ee99e78b8d4ce466a
th0rex <c0untlizzi@gmail.com>
2018-04-29 16:37:36 +0200

    make code shorter

--- src/16_2_elgamal.cpp
+++ src/16_2_elgamal.cpp
@@ -4,27 +4,32 @@
 
 using namespace math;
 
+template <typename T, typename U, typename V>
+T sm(T b, U e, V m) {
+  return sqm<true>(b, e, m);
+}
+
 void elgamal(unsigned _p, unsigned _a, unsigned _d, unsigned _i, unsigned _x) {
   trace("Verschlüsseln: \\\\\n");
   const auto al = aligned();
 
   variable p{_p, "p"}, a{_a, "\\alpha"}, d{_d, "d"}, i{_i, "i"}, x{_x, "x"};
 
-  const auto kpub_b = rename(sqm<true>(a, d, p), "k_{pub, B}");
+  const auto kpub_b = rename(sm(a, d, p), "k_{pub, B}");
   al.b();
-  const auto kpub_a = rename(sqm<true>(a, i, p), "k_{pub, A}");
+  const auto kpub_a = rename(sm(a, i, p), "k_{pub, A}");
   al.b();
-  const auto k_m = rename(sqm<true>(kpub_b, i, p), "k_M");
+  const auto k_m = rename(sm(kpub_b, i, p), "k_M");
   al.b();
   const auto y = rename((x * k_m) % p, "y");
   al.close();
 
   trace("Entschlüsseln: \\\\\n");
   al.open();
-  const auto k_m2 = rename(sqm<true>(kpub_a, d, p), "k_M");
+  const auto k_m2 = rename(sm(kpub_a, d, p), "k_M");
   al.close();
 
-  auto [_, _t, __] = eea<true, std::int64_t>(p.v(), k_m2.v());
+  const auto [_, _t, __] = eea<true, std::int64_t>(p.v(), k_m2.v());
 
   al.open();
   auto t = variable{_t, "k_M^{-1}"};
